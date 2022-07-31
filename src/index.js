4d4b74ea82a98e8552fcc08e7a7e4842033a6b79
Felipe Freitas <afelipe.freitas@gmail.com>
2022-07-30 23:49:18 -0300

    done

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Vue with Webpack and Babel';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
